cb2d614ce4ca26703298255315609967819ab3c5
M. Ian Graham <games@miangraham.com>
2013-01-16 20:58:46 -0800

    Cleanup, terminal support

--- keys.el
+++ keys.el
@@ -1,11 +1,14 @@
-;; Key Bindings
-;(global-set-key (kbd "TAB") 'self-insert-command)
-(package-require-or-install 'ace-jump-mode)
-
 (global-set-key "\C-x\C-k" 'kill-region)
 (global-set-key "\C-xg" 'goog)
 (global-set-key "\C-xp" 'phpref)
 (global-set-key "\C-xj" 'jsref)
 (global-set-key "\C-xw" 'copy-whole-buffer)
 (global-set-key [f5] 'slime-js-reload)
+
+(package-require-or-install 'ace-jump-mode)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+(setq select-active-regions nil)
+(setq mouse-drag-copy-region 't)
+(setq x-select-enable-clipboard nil)
+(global-set-key (kbd "<mouse-2>") 'mouse-yank-at-click)
--- modes.el
+++ modes.el
@@ -1,3 +1,5 @@
+(package-require-or-install 'iedit)
+
 (package-require-or-install 'zenburn-theme)
 (load-theme 'zenburn t)
 
@@ -10,9 +12,10 @@
 (setq mac-command-modifier 'meta)
 (setq cursor-type 'box)
 (blink-cursor-mode -1)
-(tool-bar-mode -1)
+(when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(unless (display-graphic-p) (menu-bar-mode -1))
 (setq scroll-bar-width 0)
-(scroll-bar-mode -1)
+(when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq visible-bell t)
 
 (global-auto-revert-mode)
@@ -24,4 +27,4 @@
 ;(add-hook 'php-mode-hook (lambda () (linum-mode 1)))
 ;(add-hook 'ruby-mode-hook (lambda () (linum-mode 1)))
 
-(set-frame-size (selected-frame) 160 60)
+(when (display-graphic-p) (set-frame-size (selected-frame) 160 60))
