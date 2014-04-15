32d514efaa4c8a2569d58aafbb61e393d2434a6f
M. Ian Graham <games@miangraham.com>
2014-04-15 15:44:43 +0900

    y-or-n and misc

--- modes.el
+++ modes.el
@@ -1,6 +1,6 @@
 (package-require-or-install 'iedit)
 (package-require-or-install 'magit)
-(setenv "NODE_ENV" "ian")
+(setenv "NODE_ENV" "development")
 
 (package-require-or-install 'zenburn-theme)
 (load-theme 'zenburn t)
@@ -17,6 +17,15 @@
 
 (iswitchb-mode 1)
 
+; (setq exec-path (split-string (getenv "PATH") ":"))
+(setenv "PATH"
+  (concat
+   "/usr/local/bin" ":"
+   (getenv "PATH")
+  )
+)
+(setq exec-path (split-string (getenv "PATH") ":"))
+
 (setq backup-directory-alist `(("." . "~/.emacs.d/backup")))
 
 (setq mac-command-modifier 'meta)
@@ -27,6 +36,9 @@
 (setq scroll-bar-width 0)
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (setq visible-bell t)
+(fset 'yes-or-no-p 'y-or-n-p)
+
+(global-discover-mode 1)
 
 (global-auto-revert-mode)
 
