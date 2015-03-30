02da61dff4d1fe7506123fdf29048bf5fb93839a
M. Ian Graham <games@miangraham.com>
2015-03-30 14:52:19 +0900

    customize cleaning

--- lisp/init-prog-setup.el
+++ lisp/init-prog-setup.el
@@ -1,6 +1,6 @@
 (require-package 'iedit)
 
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+(setq vc-handled-backends (quote (Git RCS CVS SVN SCCS Hg Mtn Arch)))
 
 (setq-default c-basic-indent 2)
 (setq-default c-basic-offset 2)
@@ -10,5 +10,7 @@
 (setq-default sh-indentation 2)
 (setq-default sh-basic-offset 2)
 (setq-default tags-file-name "TAGS")
+
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (provide 'init-prog-setup)
