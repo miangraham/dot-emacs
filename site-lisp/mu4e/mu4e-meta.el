bf8f20aa34724589460144462101e8d824fdf37b
M. Ian Graham <games@miangraham.com>
2015-05-22 19:25:36 +0900

    mu4e

---
+++ site-lisp/mu4e/mu4e-meta.el
@@ -0,0 +1,8 @@
+;; auto-generated
+(defconst mu4e-mu-version "0.9.12"
+  "Required mu binary version; mu4e's version must agree with this.")
+
+(defconst mu4e-builddir "/private/tmp/mu20150517-25089-4bw99o"
+  "Top-level build directory.")
+
+(provide 'mu4e-meta)
