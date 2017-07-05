8612e4f4db307d860d88dae62f588f6ed0a4becf
M. Ian Graham <games@miangraham.com>
2017-07-06 05:32:53 +0900

    ts and js fixes

--- lisp/init-typescript.el
+++ lisp/init-typescript.el
@@ -3,6 +3,7 @@
   :config
   (use-package tide
     :config
+    (setq tide-tsserver-executable "node_modules/typescript/bin/tsserver")
     (setq tide-tsserver-process-environment '("TSS_LOG=-level verbose -file /tmp/tss.log"))
     (setq tide-format-options '(:InsertSpaceAfterFunctionKeywordForAnonymousFunctions nil :PlaceOpenBraceOnNewLineForFunctions nil :InsertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets nil :InsertSpaceAfterOpeningAndBeforeClosingNonemptyBraces nil))
     (add-hook 'typescript-mode-hook (lambda ()
