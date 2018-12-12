b56c3f83f0b02161ab8a5d480bdd44868a08c0c5
M. Ian Graham <games@miangraham.com>
2018-12-12 20:03:06 +0900

    init packages tweak

--- lisp/init-packages.el
+++ lisp/init-packages.el
@@ -1,7 +1,7 @@
 (require 'package)
 
 (setq package-enable-at-startup nil
-      package-archives '(("melpa" . "https://melpa.org/packages/")
+      package-archives '(("melpa" . "https://www.mirrorservice.org/sites/melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/"))
       package-pinned-packages '(("magit" . "melpa")
                                 ("yasnippet" . "melpa")
@@ -13,6 +13,9 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+
+(unless (package-installed-p 'diminish)
+  (package-install 'diminish))
 
 (defalias 'after-load 'with-eval-after-load)
 
