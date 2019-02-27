b22820c253ab00494adc088010699616e1ef60af
M. Ian Graham <games@miangraham.com>
2019-02-27 18:06:32 +0900

    ps indentation borked, argh

--- custom.el
+++ custom.el
@@ -4,22 +4,25 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("d5de5ffdc352e765d4cdf02716941d932b9587dc2f768912e123cde24221b77e" "f0b0710b7e1260ead8f7808b3ee13c3bb38d45564e369cbe15fc6d312f0cd7a0" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "e6d83e70d2955e374e821e6785cd661ec363091edf56a463d0018dc49fbc92dd" "cea6d15a8333e0c78e1e15a0524000de69aac2afa7bb6cf9d043a2627327844e" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "c7cd81771525ff66c105413134cdf0330b0b5b88fd8096e5d56b0256872ba6c7" default))
- '(magit-merge-arguments '("--ff-only"))
- '(org-bullets-bullet-list '("‣" "✸" "✿" "♪"))
- '(org-bullets-face-name '##)
+   (quote
+    ("d5de5ffdc352e765d4cdf02716941d932b9587dc2f768912e123cde24221b77e" "f0b0710b7e1260ead8f7808b3ee13c3bb38d45564e369cbe15fc6d312f0cd7a0" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "3b819bba57a676edf6e4881bd38c777f96d1aa3b3b5bc21d8266fa5b0d0f1ebf" "e6d83e70d2955e374e821e6785cd661ec363091edf56a463d0018dc49fbc92dd" "cea6d15a8333e0c78e1e15a0524000de69aac2afa7bb6cf9d043a2627327844e" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "987b709680284a5858d5fe7e4e428463a20dfabe0a6f2a6146b3b8c7c529f08b" "3d5ef3d7ed58c9ad321f05360ad8a6b24585b9c49abcee67bdcbb0fe583a6950" "58c6711a3b568437bab07a30385d34aacf64156cc5137ea20e799984f4227265" "46fd293ff6e2f6b74a5edf1063c32f2a758ec24a5f63d13b07a20255c074d399" "2b5aa66b7d5be41b18cc67f3286ae664134b95ccc4a86c9339c886dfd736132d" "c7cd81771525ff66c105413134cdf0330b0b5b88fd8096e5d56b0256872ba6c7" default)))
+ '(magit-merge-arguments (quote ("--ff-only")))
+ '(org-bullets-bullet-list (quote ("‣" "✸" "✿" "♪")))
+ '(org-bullets-face-name (quote ##))
  '(org-tree-slide-activate-message "")
  '(org-tree-slide-deactivate-message "")
  '(org-tree-slide-never-touch-face nil)
  '(package-selected-packages
-   '(paradox org-randomnote mozc org-bullets org-present epresent moom org-tree-slide nocomments-mode tidal reason-mode ranger psci purescript-indentation purescript-mode elfeed intero yasnippet projectile shackle counsel-projectile counsel cider clojure-mode elixir-mode fireplace all-the-icons-ivy all-the-icons prettier-js go-mode tide fsharp-mode typescript-mode dumb-jump org-board groovy-mode highlight-indent-guides better-shell stopwatch mode-icons youtube-dl-mode inityaml lisp/init-yaml init-yaml init-ivy mu4e/mu4e mu4e haskell-mode use-package org json-mode ag ivy nginx-mode move-dup csharp-mode ztree zenburn-theme yaml-mode ws-butler wiki-summary which-key web-mode totd sunshine spaceline smartparens rust-mode rainbow-delimiters php-mode org-projectile names markdown-mode magit ledger-mode kotlin-mode js2-mode java-snippets iedit hydra git-timemachine git-link fullframe frame-cmds flycheck exec-path-from-shell esup elfeed-org editorconfig-core editorconfig dtrt-indent dired-details diminish define-word dash-at-point calfw auto-complete anzu aggressive-indent ace-window ace-jump-mode))
+   (quote
+    (dhall-mode forge doom-modeline paradox org-randomnote mozc org-bullets org-present epresent moom org-tree-slide nocomments-mode tidal reason-mode ranger psci purescript-indentation purescript-mode elfeed intero yasnippet projectile shackle counsel-projectile counsel cider clojure-mode elixir-mode fireplace all-the-icons-ivy all-the-icons prettier-js go-mode tide fsharp-mode typescript-mode dumb-jump org-board groovy-mode highlight-indent-guides better-shell stopwatch mode-icons youtube-dl-mode inityaml lisp/init-yaml init-yaml init-ivy mu4e/mu4e mu4e haskell-mode use-package org json-mode ag ivy nginx-mode move-dup csharp-mode ztree zenburn-theme yaml-mode ws-butler wiki-summary which-key web-mode totd sunshine spaceline smartparens rust-mode rainbow-delimiters php-mode org-projectile names markdown-mode magit ledger-mode kotlin-mode js2-mode java-snippets iedit hydra git-timemachine git-link fullframe frame-cmds flycheck exec-path-from-shell esup elfeed-org editorconfig-core editorconfig dtrt-indent dired-details diminish define-word dash-at-point calfw auto-complete anzu aggressive-indent ace-window ace-jump-mode)))
  '(paradox-github-token t)
  '(safe-local-variable-values
-   '((tide-tsserver-executable . "../../node_modules/typescript/bin/tsserver")
+   (quote
+    ((tide-tsserver-executable . "../../node_modules/typescript/bin/tsserver")
      (dtrt-indent-mode)
      (dtrt-indent-mode . 0)
      (dtrt-indent-explicit-offset . t)
-     (dtrt-indent-explicit-offset . 0))))
+     (dtrt-indent-explicit-offset . 0)))))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- lisp/init-purescript.el
+++ lisp/init-purescript.el
@@ -1,7 +1,20 @@
 (use-package purescript-mode
   :mode ("\\.purs?\\'" . purescript-mode)
   :config
-  (add-hook 'purescript-mode-hook 'turn-on-purescript-indent)
+  (add-hook 'purescript-mode-hook 'ps-hook)
+  ;; (add-hook 'purescript-mode-hook 'turn-on-purescript-indent)
+  )
+
+(defun ps-hook ()
+  (turn-on-purescript-simple-indent)
+  (define-key purescript-mode-map (kbd "C-<right>")
+    (lambda ()
+      (interactive)
+      (purescript-move-nested 1)))
+  (define-key purescript-mode-map (kbd "C-<left>")
+    (lambda ()
+      (interactive)
+      (purescript-move-nested -1)))
   )
 
 (use-package psci)
