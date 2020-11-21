2cdf08ef37b9ed16acb6682863f9e3d6d5b329b8
M. Ian Graham <games@miangraham.com>
2020-11-21 22:02:32 +0900

    font tweaks

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -1,3 +1,10 @@
+(defvar my/vfont "Fantasque Sans Mono")
+
+(use-package unicode-fonts
+  :defer 1
+  :config
+  (unicode-fonts-setup))
+
 (use-package doom-themes
   :defer 0
   :custom
@@ -5,8 +12,8 @@
   (doom-Iosvkem-padded-modeline 2)
   :config
   (when (display-graphic-p)
-    (set-face-font 'default "Fantasque Sans Mono")
-    (set-frame-font "Fantasque Sans Mono")
+    (set-face-font 'default my/vfont)
+    (set-frame-font my/vfont)
     (set-fontset-font t 'japanese-jisx0208 (font-spec :family "Noto Sans CJK JP Bold")))
 
   (if (display-graphic-p)
@@ -19,7 +26,7 @@
   :custom
   (solaire-mode-auto-swap-bg t)
   :config
-  (set-face-font 'solaire-default-face "Fantasque Sans Mono")
+  (set-face-font 'solaire-default-face my/vfont)
   (set-face-attribute 'solaire-default-face nil :height (if (string= (system-name) "nanachi") 160 200))
 
   (setq solaire-mode-remap-alist
