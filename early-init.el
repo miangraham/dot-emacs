d0e2d4fe7edfbc4ac96b9b39f617ef34d26723b5
M. Ian Graham <games@miangraham.com>
2020-11-21 22:12:17 +0900

    silence cl deprecation warning

--- early-init.el
+++ early-init.el
@@ -1,5 +1,4 @@
 (setq gc-cons-threshold (* 100 1000 1000))
-
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("gnu" . "https://elpa.gnu.org/packages/"))
       package-pinned-packages '(("magit" . "melpa")
@@ -10,3 +9,5 @@
 (when (fboundp 'menu-bar-mode) (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+(setq byte-compile-warnings '(not obsolete))
