dd5ad4a7cae28b94f718792c70ca970738ec8df2
M. Ian Graham <games@miangraham.com>
2021-02-28 00:48:47 +0900

    shackle tweak

--- lisp/init-shackle.el
+++ lisp/init-shackle.el
@@ -2,10 +2,11 @@
   :ensure
   :diminish
   :custom
-  (shackle-rules '(("*Messages*" :select nil :other t :align bottom :size 0.2 :inhibit-window-quit t)
+  (shackle-rules '(("*Messages*" :select nil :other t :align bottom :size 0.2 :inhibit-window-quit nil)
                    ;; (".*magit.*" :regexp t :frame t :same t :select t)
 		   ("*Buffer List*" :select t :same t)))
   (shackle-default-rule nil)
+  (shackle-inhibit-window-quit-on-same-windows t)
   :config
   (shackle-mode))
 
